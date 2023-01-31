01f4193d77b1af2b2fe673661261b115f5c732ff
lolamartvarela <luciamvarela@outlook.com>
2023-01-31 17:59:01 +0000

    error 400

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,132 +1,126 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
+
 // create your first component
 const Home = () => {
-  const [Datos, setDatos] = useState("");
-  const [save, setSave] = useState([]);
+    const [Datos, setDatos] = useState("");
+    const [save, setSave] = useState([]);
 
-/*function handleDatos(e) {
-    setDatos(e.target.value);
-  }*/
+    function crearUsuario() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify([])
+        }).then((response) => response.json()).then((data) => console.log(data))
+    }
 
 
-  function enviarDatos(e) {
-    e.preventDefault();
-    setSave(save.concat({label: Datos, done: false}));
-    setDatos("");
-  }
+    function enviarDatos(e) {
+        e.preventDefault();
+        setSave(save.concat(Datos));
+        setDatos("");
+    }
 
-  function eliminarDatos (eliminarli) {
-    const nuevasTareas = save.filter(function (item, index) {
-      return index !== eliminarli;
-    });
+    const eliminarDatos = (indexItem) => {
+      setSave((prevState) =>
+        prevState.filter((saveItems, index) => index !== indexItem)
+      );
+      };
 
-    setSave(nuevasTareas);
-  }
-
-  function crearUsuario (){
-    fetch ("https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela", {
-        method:"POST",
-        headers: {
-            "Content-Type":"application/json"
-         },
-         body: JSON.stringify ([]),
-    })
-      .then ((response) => response.json ())
-      .then ((data) => console.log (data))
-}
-
-function obtenerTodoList (){
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela", {
+    function obtenerDatos() {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela', {
         method:"GET",
-/*headers:{
-            "Content-Type":"application/json"
-         },*/
     })
+
       .then ((response) => response.json ())
       .then ((data) => console.log (data))
 }
 
-function actualizarLista (){
-    fetch ("https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela", {
-        method:"PUT",
-        headers: {
-            "Content-Type":"application/json"
-         },
-         body: {label: Datos, done: false}
-    })
-      .then ((response) => response.json ())
-      .then ((data) => console.log (data))
-}
-
-/*function eliminarLista (){
-    fetch ("https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela", {
+    function borrarDatos() {
+      fetch('https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela', {
         method:"DELETE",
-        headers: {
-            "Content-Type":"application/json"
-         },
-         
     })
+
       .then ((response) => response.json ())
-      .then ((data) => console.log (data))
-}*/
+      .then ((data) => console.log (data.results))
+      if (data.results === "ok"){
+        setSave([])
+      }
+    }
 
-  useEffect (() => {
-//crearUsuario ();
-obtenerTodoList ();
-//actualizarLista ();
-//eliminarLista();
-  }, []);
+    function actualizarDatos() {
+      fetch('https://assets.breatheco.de/apis/fake/todos/user/lolamartvarela', {
+        method:"PUT",
+    })
 
+    .then ((response) => response.json ())
+    .then ((data) => console.log (data))
+    }
 
-  return (
-    <>
-      <div>
-        <p className="fst-italic fw-light fs-1 d-flex justify-content-center col-md-4 offset-md-4 mt-4">
-          todos
-        </p>
-      </div>
-      <div className="container col-md-4 offset-md-4 mt-4">
-        <div className="input-group mb-3 mt-3">
-          <input
-            id="exampleDatoss"
-            onChange={(e) => {
-              setDatos(e.target.value);
-            }}
-            type="text"
-            className="form-control"
-            aria-describedby="button-addon2"
-            value={Datos}
-          />
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="enviar"
-            onClick={enviarDatos}
-          >
-            Enviar
-          </button>
-        </div>
+    useEffect(() => {
+      // crearUsuario();
+      obtenerDatos()
+    }, [])
 
-        <ul>
+    useEffect (() => {
+      actualizarDatos()
+    }), [save]
+    
+    return (
+        <>
+            <div className="container-fluid border col-md-8 mt-4">
+                <div>
+                    <p className="fst-italic fw-light fs-1 d-flex  justify-content-center col-md-4 offset-md-4 mt-4">
+                        todos.
+                    </p>
+                </div>
+                <div className="container col-md-4 offset-md-4 mt-4">
+                    <div className="input-group mb-3 mt-3">
+                        <input id="exampleDatos"
+                            onChange={
+                                (e) => {
+                                    setDatos(e.target.value);
+                                }
+                            }
+                            type="text"
+                            className="form-control"
+                            aria-describedby="inputHelp"
+                            value={Datos}/>
 
-          {save.map((item, index) => (
-            <li className="list-group-item" key={index}>
-              {item}
-              <button
-                className="btn"
-                type="button"
-                onClick={() => eliminarDatos(index)}
-              >
-                <i className="fas fa-trash-alt" />
-              </button>
-            </li>
-          ))}{" "}
-        
-        </ul>
-        </div>
-      
-    </>
-  );
+                        <button className="btn btn-outline-secondary" type="submit" id="enviar"
+                            onClick={enviarDatos}>
+                            Enviar
+                        </button>
+                    </div>
+
+                    <ul> {
+                        save.map((item, index) => (
+                            <li className="list-group-item"
+                                key={index}>
+                                {item}
+                                <button className="btn" type="button"
+                                    onClick={
+                                        () => eliminarDatos(index)
+                                }>
+                                    <i className="fas fa-trash-alt"/>
+                                </button>
+                            </li>
+                        ))
+                    }
+                        {" "} </ul>
+
+                    <div>
+                      <div className="delete-lista d-flex justify-content-center mt-3 md-w50 mb-2">
+                      <button type="submit" className="btn border btn-sm mt-2" onClick={borrarDatos}>Borrar Lista</button>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
+
+
